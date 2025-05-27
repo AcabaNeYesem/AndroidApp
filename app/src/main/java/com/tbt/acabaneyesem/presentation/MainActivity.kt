3ea9aec0bf643b1763f80c7c82c819981e9d9e3c
Korugan32 <tahacansenel@gmail.com>
2025-05-27 16:59:13 +0300

    Implement basic navigation
    The `MainActivity` is now annotated with `@AndroidEntryPoint` and uses `Navigation()` for content.

--- app/src/main/java/com/tbt/acabaneyesem/presentation/MainActivity.kt
+++ app/src/main/java/com/tbt/acabaneyesem/presentation/MainActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.ExperimentalMaterial3Api
+import com.tbt.acabaneyesem.presentation.common.navigation.Navigation
 import com.tbt.acabaneyesem.presentation.ui.theme.AcabaNeYesemTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AcabaNeYesemTheme {
-
+                Navigation()
             }
         }
     }
